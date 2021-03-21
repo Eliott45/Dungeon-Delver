28531a3ff7eff5408d8869be9219ca94c93a4d97
Eliott <eliott_nu@mail.ru>
2021-03-21 14:43:09 +0900

    Movement of player

---
+++ Dungeon Delver/Assets/__Scripts/Dray.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dray : MonoBehaviour
+{
+    [Header("Set in Inpector")]
+    public float speed = 5f;
+
+    [Header("Set Dynamically")]
+    public int dirHeld = -1; // Направление, соответствующее удерживаемой клавише
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>(); 
+    }
+
+    private void Update()
+    {
+        // Задать направление персонажу 
+        dirHeld = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) dirHeld = 0;
+        if (Input.GetKey(KeyCode.UpArrow)) dirHeld = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
+        if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
+
+        Vector3 vel = Vector3.zero; 
+        switch(dirHeld)
+        {
+            case 0:
+                vel = Vector3.right;
+                break;
+            case 1:
+                vel = Vector3.up;
+                break;
+            case 2:
+                vel = Vector3.left;
+                break;
+            case 3:
+                vel = Vector3.down;
+                break;
+        }
+
+        rigid.velocity = vel * speed;
+    }
+}
