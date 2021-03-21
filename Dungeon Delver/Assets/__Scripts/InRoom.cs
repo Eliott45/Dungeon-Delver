90cd09d1bd3f31547b97f810d695c568988940c6
Eliott <eliott_nu@mail.ru>
2021-03-22 02:08:10 +0900

    Script InRoom

---
+++ Dungeon Delver/Assets/__Scripts/InRoom.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InRoom : MonoBehaviour
+{
+    static public float ROOM_W = 16; // Ширина комнаты
+    static public float ROOM_H = 11; // Высота комнаты
+    static public float WALL_T = 2;  // Толщина стен
+}
