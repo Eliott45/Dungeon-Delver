b14d61bb676e6cdf35bfaf7b9f78c3d78c823bff
Eliott <eliott_nu@mail.ru>
2021-03-21 19:30:26 +0900

    Attack animation programming

--- Dungeon Delver/Assets/__Scripts/Dray.cs
+++ Dungeon Delver/Assets/__Scripts/Dray.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Dray : MonoBehaviour
 {
+    public enum eMode { idle, move, attack, transition };
+
     [Header("Set in Inpector")]
-    public float speed = 5f;
+    public float speed = 5f; // Скорость передвижения
+    public float attackDuration = 0.25f; // Продолжительность атаки
+    public float attackDelay = 0.5f; // Задержка между атаками
 
     [Header("Set Dynamically")]
     public int dirHeld = -1; // Направление, соответствующее удерживаемой клавише
+    public int facing = 1; // Направление движения Дрея
+    public eMode mode = eMode.idle;
+
+    private float timeAtkDone = 0; // Время, когда должна завершиться анимация атаки
+    private float timeAtkNext = 0; // Время, когда Дрей сможет повторить атаку
     private Rigidbody rigid;
     private Animator anim;
     private Vector3[] directions = new Vector3[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
@@ -22,8 +31,8 @@ public class Dray : MonoBehaviour
 
     private void Update()
     {
-        // Задать направление персонажу 
-        dirHeld = -1;
+        //----Обработка ввода с клавиатуры и управление режимами eMode----\\
+        dirHeld = -1; // Задать направление персонажу 
         for (int i = 0; i < 4; i++)
         {
             if (Input.GetKey(keys[i])) dirHeld = i;
@@ -35,6 +44,33 @@ public class Dray : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
         if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
         */
+
+        // Нажата клавиша атаки
+        if(Input.GetKeyDown(KeyCode.Z) && Time.time >= timeAtkDone) // Если нажата клавиша "Z" и прошло достаточко много времени после предыдущей атаки
+        {
+            mode = eMode.attack;
+            timeAtkDone = Time.time + attackDuration;
+            timeAtkNext = Time.time + attackDelay;
+        }
+
+        // Завершить атаку, если время истекло
+        if(Time.time >= timeAtkDone)
+        {
+            mode = eMode.idle;
+        }
+
+        // Выбрать правильный режим, если дрей не атакует
+        if (mode != eMode.attack)
+        {
+            if (dirHeld == -1)
+            {
+                mode = eMode.idle;
+            } else
+            {
+                facing = dirHeld;
+                mode = eMode.move;
+            }
+        }
 
         Vector3 vel = Vector3.zero;
 
@@ -56,19 +92,24 @@ public class Dray : MonoBehaviour
         }
         */
 
-
-        if (dirHeld > -1) vel = directions[dirHeld];
+        switch(mode)
+        {
+            case eMode.attack:
+                anim.CrossFade("Dray_Attack_" + facing, 0);
+                anim.speed = 0;
+                break;
+            case eMode.idle:
+                anim.CrossFade("Dray_Walk_" + facing, 0); // Переключить анимациию
+                anim.speed = 0;
+                break;
+            case eMode.move:
+                vel = directions[dirHeld];
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 1;
+                break;
+        }
 
         rigid.velocity = vel * speed;
 
-        // Анимация 
-        if(dirHeld == -1)
-        {
-            anim.speed = 0;
-        } else
-        {
-            anim.CrossFade("Dray_Walk_"+dirHeld, 0); // Переключить анимациию
-            anim.speed = 1;
-        }
     }
 }
