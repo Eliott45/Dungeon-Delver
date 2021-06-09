f2292698bc15d0cc5034a755d425379d38c38595
Eliott <eliott_nu@mail.ru>
2021-06-09 16:31:53 +0900

    Refactoring script GateKeeper

--- Dungeon Delver/Assets/__Scripts/GateKeeper.cs
+++ Dungeon Delver/Assets/__Scripts/GateKeeper.cs
@@ -9,20 +9,20 @@ namespace __Scripts
         private AudioSource _aud;
 
         //----- Иднексы плиток с запертыми дверьми
-        const int lockedR = 95;
-        const int lockedUR = 81;
-        const int lockedUL = 80;
-        const int lockedL = 100;
-        const int lockedDL = 101;
-        const int lockedDR = 102;
+        private const int LockedR = 95;
+        private const int LockedUr = 81;
+        private const int LockedUl = 80;
+        private const int LockedL = 100;
+        private const int LockedDL = 101;
+        private const int LockedDr = 102;
 
         //----- Индексы плиток с открытыми дверьми
-        const int openR = 48;
-        const int openUR = 93;
-        const int openUL = 92;
-        const int openL = 51;
-        const int openDL = 26;
-        const int openDR = 27;
+        private const int OpenR = 48;
+        private const int OpenUr = 93;
+        private const int OpenUl = 92;
+        private const int OpenL = 51;
+        private const int OpenDL = 26;
+        private const int OpenDr = 27;
 
         private IKeyMaster keys;
 
@@ -38,46 +38,46 @@ namespace __Scripts
             if (keys.KeyCount < 1) return;
 
             // Интерес представляют только плитки
-            Tile ti = coll.gameObject.GetComponent<Tile>();
+            var ti = coll.gameObject.GetComponent<Tile>();
             if (ti == null) return;
 
             // Открывать, только если дрей обращен лицом к двери (предотвратить случайно использованик ключа)
-            int facing = keys.GetFacing();
+            var facing = keys.GetFacing();
             // Проверить, является ли плитка закрытой дверью
             Tile ti2;
             switch(ti.tileNum)
             {
-                case lockedR:
+                case LockedR:
                     if (facing != 0) return;
-                    ti.SetTile(ti.x, ti.y, openR);
+                    ti.SetTile(ti.x, ti.y, OpenR);
                     break;
-                case lockedUR:
+                case LockedUr:
                     if (facing != 1) return;
-                    ti.SetTile(ti.x, ti.y, openUR);
+                    ti.SetTile(ti.x, ti.y, OpenUr);
                     ti2 = TileCamera.TILES[ti.x - 1, ti.y];
-                    ti2.SetTile(ti2.x, ti2.y, openUL);
+                    ti2.SetTile(ti2.x, ti2.y, OpenUl);
                     break;
-                case lockedUL:
+                case LockedUl:
                     if (facing != 1) return;
-                    ti.SetTile(ti.x, ti.y, openUL);
+                    ti.SetTile(ti.x, ti.y, OpenUl);
                     ti2 = TileCamera.TILES[ti.x + 1, ti.y];
-                    ti2.SetTile(ti2.x, ti2.y, openUR);
+                    ti2.SetTile(ti2.x, ti2.y, OpenUr);
                     break;
-                case lockedL:
+                case LockedL:
                     if (facing != 2) return;
-                    ti.SetTile(ti.x, ti.y, openL);
+                    ti.SetTile(ti.x, ti.y, OpenL);
                     break;
-                case lockedDL:
+                case LockedDL:
                     if (facing != 3) return;
-                    ti.SetTile(ti.x, ti.y, openDL);
+                    ti.SetTile(ti.x, ti.y, OpenDL);
                     ti2 = TileCamera.TILES[ti.x + 1, ti.y];
-                    ti2.SetTile(ti2.x, ti2.y, openDR);
+                    ti2.SetTile(ti2.x, ti2.y, OpenDr);
                     break;
-                case lockedDR:
+                case LockedDr:
                     if (facing != 3) return;
-                    ti.SetTile(ti.x, ti.y, openDR);
+                    ti.SetTile(ti.x, ti.y, OpenDr);
                     ti2 = TileCamera.TILES[ti.x - 1, ti.y];
-                    ti2.SetTile(ti2.x, ti2.y, openDL);
+                    ti2.SetTile(ti2.x, ti2.y, OpenDL);
                     break;
                 default:
                     return; // Выйти, чтобы исключить уменьшение счетчика ключей
